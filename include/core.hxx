677c662794f48e81999a49ec86f2dd8f433fdb43
Astral-Error <182073205+Astral-Error@users.noreply.github.com>
2025-07-12 00:51:00 +0530

    change: introduced a vector of type inGameObject to enable multiple in-game objects to render and update positions in the game loop.

--- include/core.hxx
+++ include/core.hxx
@@ -1,9 +1,12 @@
 #pragma once
 #include "window.hxx"
+#include "inGameObject.hxx"
+#include <vector>
 
 class core{
     private:
         window win;
+        std::vector<inGameObject> gameObjects;
     
     public:
         bool initiateWindow(const char*,int,int);
--- src/engine/core.cxx
+++ src/engine/core.cxx
@@ -1,22 +1,35 @@
 #include "core.hxx"
 #include "inGameObject.hxx"
+#include "time.hxx"
 #include <SDL.h>
 #include <iostream>
+#include <vector>
 
 bool core::initiateWindow(const char* winTitle, int width, int height){
     return win.initiateWindow(winTitle,width,height);
 }
 
 void core::initiateGameLoop(){
-    inGameObject object_Rect(100,100,50);   
+    gameObjects.emplace_back(100,100,50);
     while(win.isRunning()){
+        engineTime::startFrame();
         win.inputHandler();
-        object_Rect.updateObjectState();
+
+        for(inGameObject& i : gameObjects){
+            i.updateObjectState(engineTime::getDeltaTime());
+        }
+
         if(SDL_SetRenderDrawColor(win.getRenderer(),150,160,255,255)<0){
             std::cout<<"Render Draw Color failed, Error: "<<SDL_GetError()<<std::endl;
         }
+
         SDL_RenderClear(win.getRenderer());
-        object_Rect.renderObject(win.getRenderer());
+
+        for(inGameObject& i : gameObjects){
+            i.renderObject(win.getRenderer());
+        }
+
         SDL_RenderPresent(win.getRenderer());
+        engineTime::endFrame(60);
     }
 }
