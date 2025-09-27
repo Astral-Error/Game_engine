46f5175034076c12d0de24cc1edcc7008868d8d8
Astral-Error <182073205+Astral-Error@users.noreply.github.com>
2025-09-28 01:41:43 +0530

    Revert "add(openGL): add openGL classes for rendering pipeline"
    This reverts commit ea0616c9fec350428e6ab2241c33567deeeb7d91.

--- include/glUtils.hxx
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <glad/glad.h>
-#include <string>
-
-namespace glutils{
-    GLuint compileShader(GLenum, const std::string&);
-    GLuint linkProgram(GLuint, GLuint);
-    bool checkGLError(const std::string&);
-}
